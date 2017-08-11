4f89724a3a0ad393cc946ae4048b5d264eb642c1
Gursimran Singh (Digital) <GU353199@wipro.com>
2017-08-11 09:48:55 +0530

    material done in registration & search ben

--- src/app/beneficiary-registration/beneficiary-registration.component.ts
+++ src/app/beneficiary-registration/beneficiary-registration.component.ts
@@ -201,6 +201,7 @@ export class BeneficiaryRegistrationComponent implements OnInit {
 
   calledEarlierCheck(flag) {
 
+    this.genderErrFlag = false;
     if (flag.checked) {
       this.calledEarlier = true;
       this.searchValue = 'Advance Search';
@@ -667,6 +668,20 @@ export class BeneficiaryRegistrationComponent implements OnInit {
     });
 
 
+  }
+    genderErrFlag: any = false;
+
+  genderchange(value) {
+    console.log(this.calledEarlier,"klkl");
+    console.log(this.notCalledEarlier,"ffff");
+    if (value == '' || value == null) {
+      this.genderErrFlag = true;
+    }
+    else {
+      this.genderErrFlag = false;
+   
+
+    }
   }
   // getLocationPerPincode(pincodeObj: any) {
   //   this.areaList = [];
