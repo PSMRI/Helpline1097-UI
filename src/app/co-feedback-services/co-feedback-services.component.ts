f378c32b8f904ad62781d35d1c2a43121d615dcf
vi314759 <vinay.chidambara@wipro.com>
2017-08-28 18:35:43 +0530

    fixed failure issue in feedback creation

--- src/app/co-feedback-services/co-feedback-services.component.ts
+++ src/app/co-feedback-services/co-feedback-services.component.ts
@@ -13,14 +13,15 @@ import { Subscription } from 'rxjs/Subscription';
 import { CommunicationService } from './../services/common/communication.service'
 
 
-@Component({
+@Component( {
   selector: 'app-co-feedback-services',
   templateUrl: './co-feedback-services.component.html',
-  styleUrls: ['./co-feedback-services.component.css']
+  styleUrls: [ './co-feedback-services.component.css' ]
 
 
-})
-export class CoFeedbackServicesComponent implements OnInit {
+} )
+export class CoFeedbackServicesComponent implements OnInit
+{
   @Input() current_language: any;
   currentlanguage: any;
 
@@ -72,45 +73,52 @@ export class CoFeedbackServicesComponent implements OnInit {
     public dialog: MdDialog,
     private alertMessage: ConfirmationDialogsService,
     private pass_data: CommunicationService
-  ) { this.subscription = this.pass_data.getData().subscribe(message => { this.getBenData(message) }); }
+  ) { this.subscription = this.pass_data.getData().subscribe( message => { this.getBenData( message ) } ); }
 
-  showForm() {
+  showForm ()
+  {
     this.showFormCondition = true;
     this.showTableCondition = false;
   }
 
-  showTable() {
+  showTable ()
+  {
     this.showFormCondition = false;
     this.showTableCondition = true;
   }
 
-  GetServiceTypes() {
-    this._feedbackTypes.getTypes(this.providerServiceMapID)
-      .subscribe(response => this.setServiceTypes(response));
+  GetServiceTypes ()
+  {
+    this._feedbackTypes.getTypes( this.providerServiceMapID )
+      .subscribe( response => this.setServiceTypes( response ) );
   }
-  setServiceTypes(response: any) {
-    for (let i: any = 0; i < response.length; i++) {
-      if (response[i].subServiceName.toUpperCase().search('FEED') >= 0) {
-        this.subServiceID = response[i].subServiceID;
+  setServiceTypes ( response: any )
+  {
+    for ( let i: any = 0; i < response.length; i++ )
+    {
+      if ( response[ i ].subServiceName.toUpperCase().search( 'FEED' ) >= 0 )
+      {
+        this.subServiceID = response[ i ].subServiceID;
         break;
       }
     }
   }
 
-  ngOnInit() {
-    this.GetServiceTypes();
+  ngOnInit ()
+  {
     this.beneficiaryRegID = this._savedData.beneficiaryData.beneficiaryRegID;
     this.userName = this._savedData.uname;
     this.serviceID = this._savedData.current_service.serviceID;
     this.providerServiceMapID = this._savedData.current_service.serviceID;
+    this.GetServiceTypes();
     this._userBeneficiaryData.getUserBeneficaryData()
-      .subscribe(response => this.SetUserBeneficiaryFeedbackData(response));
+      .subscribe( response => this.SetUserBeneficiaryFeedbackData( response ) );
     this._coFeedbackService.getDesignations()
-      .subscribe(response => this.setDesignation(response));
+      .subscribe( response => this.setDesignation( response ) );
     this._feedbackTypes.getFeedbackTypesData()
-      .subscribe(response => this.setFeedbackTypes(response));
+      .subscribe( response => this.setFeedbackTypes( response ) );
     this._feedbackTypes.getFeedbackSeverityData()
-      .subscribe(response => this.setFeedbackSeverity(response));
+      .subscribe( response => this.setFeedbackSeverity( response ) );
     this.showBeneficiaryFeedbackList();
     this.count = '0/300';
 
@@ -120,98 +128,119 @@ export class CoFeedbackServicesComponent implements OnInit {
 
 
   // tslint:disable-next-line:use-life-cycle-interface
-  ngOnChanges() {
-    this.setLanguage(this.current_language);
+  ngOnChanges ()
+  {
+    this.setLanguage( this.current_language );
 
   }
 
-  setLanguage(language) {
+  setLanguage ( language )
+  {
     this.currentlanguage = language;
-    console.log(language, 'language feedback services mein');
+    console.log( language, 'language feedback services mein' );
   }
 
-  showBeneficiaryFeedbackList() {
-    this._coFeedbackService.getFeedbackHistoryById(this.beneficiaryRegID, this.serviceID)
-      .subscribe((response) => {
-        this.setFeedbackHistoryByID(response)
+  showBeneficiaryFeedbackList ()
+  {
+    this._coFeedbackService.getFeedbackHistoryById( this.beneficiaryRegID, this.serviceID )
+      .subscribe(( response ) =>
+      {
+        this.setFeedbackHistoryByID( response )
         this.showTable();
-      }, (err) => {
-        this.alertMessage.alert('Some Internal Error');
-        console.log('Error in fetching Data of FeedBack');
-      });
+      }, ( err ) =>
+      {
+        this.alertMessage.alert( 'Some Internal Error' );
+        console.log( 'Error in fetching Data of FeedBack' );
+      } );
 
 
   }
-  SetUserBeneficiaryFeedbackData(regData: any) {
-    if (regData.states) {
+  SetUserBeneficiaryFeedbackData ( regData: any )
+  {
+    if ( regData.states )
+    {
       this.states = regData.states;
     }
   }
 
-  GetDistricts(state: number) {
+  GetDistricts ( state: number )
+  {
     this.districts = [];
     this.taluks = [];
     this.blocks = [];
     this.institutes = [];
-    this._locationService.getDistricts(state)
-      .subscribe(response => this.SetDistricts(response));
+    this._locationService.getDistricts( state )
+      .subscribe( response => this.SetDistricts( response ) );
   }
-  SetDistricts(response: any) {
+  SetDistricts ( response: any )
+  {
     this.districts = response;
   }
-  GetTaluks(district: number) {
+  GetTaluks ( district: number )
+  {
     this.taluks = [];
     this.blocks = [];
     this.institutes = [];
-    this._locationService.getTaluks(district)
-      .subscribe(response => this.SetTaluks(response));
+    this._locationService.getTaluks( district )
+      .subscribe( response => this.SetTaluks( response ) );
   }
-  SetTaluks(response: any) {
+  SetTaluks ( response: any )
+  {
     this.taluks = response;
   }
-  GetBlocks(taluk: number) {
+  GetBlocks ( taluk: number )
+  {
     this.blocks = [];
     this.institutes = [];
-    this._locationService.getBranches(taluk)
-      .subscribe(response => this.SetBlocks(response));
+    this._locationService.getBranches( taluk )
+      .subscribe( response => this.SetBlocks( response ) );
   }
-  SetBlocks(response: any) {
+  SetBlocks ( response: any )
+  {
     this.blocks = response;
   }
 
-  GetInstitutes() {
+  GetInstitutes ()
+  {
     this.institutes = [];
     let object = { 'stateID': this.selected_state, 'districtID': this.selected_district, 'districtBranchMappingID': this.selected_sdtb };
-    this._locationService.getInstituteList(object)
-      .subscribe(response => this.SetInstitutes(response));
+    this._locationService.getInstituteList( object )
+      .subscribe( response => this.SetInstitutes( response ) );
   }
-  SetInstitutes(response: any) {
+  SetInstitutes ( response: any )
+  {
     this.institutes = response.institute;
   }
 
-  setDesignation(response: any) {
+  setDesignation ( response: any )
+  {
     this.designations = response;
   }
-  setFeedbackTypes(response: any) {
+  setFeedbackTypes ( response: any )
+  {
     this.feedbackTypes = response;
   }
 
-  setFeedbackSeverity(response: any) {
+  setFeedbackSeverity ( response: any )
+  {
     this.feedbackSeverities = response;
   }
 
 
-  generatefeedbackID() {
+  generatefeedbackID ()
+  {
     return this.feedbackcounter++;
   }
 
   // submitFeedback ( object: any )
-  submitFeedback() {
+  submitFeedback ()
+  {
     let selected_Date;
-    if (this.selected_doi) {
-      selected_Date = new Date((this.selected_doi) - 1 * (this.selected_doi.getTimezoneOffset() * 60 * 1000)).toJSON();
+    if ( this.selected_doi )
+    {
+      selected_Date = new Date(( this.selected_doi ) - 1 * ( this.selected_doi.getTimezoneOffset() * 60 * 1000 ) ).toJSON();
     }
-    const feedbackObj = [{
+    const feedbackObj = [ {
       'institutionID': this.selected_institution,
       'designationID': this.selected_designation,
       'severityID': this.selected_severity,
@@ -226,15 +255,17 @@ export class CoFeedbackServicesComponent implements OnInit {
       'benCallID': this._savedData.callData.benCallID,
       '1097ServiceID': this.serviceID
     }];
-    this._coFeedbackService.createFeedback(feedbackObj)
-      .subscribe((response) => {
-        this.alertMessage.alert('Successfully Created');
+    this._coFeedbackService.createFeedback( feedbackObj )
+      .subscribe(( response ) =>
+      {
+        this.alertMessage.alert( 'Successfully Created' );
         this.showBeneficiaryFeedbackList()
-      }, (err) => {
+      }, ( err ) =>
+      {
         this.selected_doi = undefined;
-        console.log('Error in Feedback', err);
-        this.alertMessage.alert('Internal Error');
-      });
+        console.log( 'Error in Feedback', err );
+        this.alertMessage.alert( 'Internal Error' );
+      } );
   }
   // showtable(response, obj) {
   //   console.log('after registering feedback', response);
@@ -264,36 +295,43 @@ export class CoFeedbackServicesComponent implements OnInit {
 
   // }
 
-  modalData(object) {
-    this.modalArray.push(object);
+  modalData ( object )
+  {
+    this.modalArray.push( object );
 
-    this.modalArray = this.modalArray.map(function (data) {
+    this.modalArray = this.modalArray.map( function ( data )
+    {
       return {
         'feedbackRequests': data.feedbackRequests,
         'feedbackResponses': data.feedbackResponses
       }
-    })
-    const dialogRef = this.dialog.open(FeedbackStatusComponent, {
+    } )
+    const dialogRef = this.dialog.open( FeedbackStatusComponent, {
       height: '90%',
       width: '80%',
       data: this.modalArray,
-    });
-    dialogRef.afterClosed().subscribe(result => {
-    });
+    } );
+    dialogRef.afterClosed().subscribe( result =>
+    {
+    } );
   }
 
-  setFeedbackHistoryByID(response: any) {
-    console.log('the response for feedback history is', response);
+  setFeedbackHistoryByID ( response: any )
+  {
+    console.log( 'the response for feedback history is', response );
     this.feedbacksArray = response;
   }
-  updateCount() {
+  updateCount ()
+  {
     this.count = this.feedbackDescription.length + '/300';
   }
-  getBenData(benData: any) {
+  getBenData ( benData: any )
+  {
     this.beneficiaryRegID = benData.dataPass.beneficiaryRegID;
   }
   // tslint:disable-next-line:use-life-cycle-interface
-  ngOnDestroy() {
+  ngOnDestroy ()
+  {
     // unsubscribe to ensure no memory leaks
     this.subscription.unsubscribe();
   }
