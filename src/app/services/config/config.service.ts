1aacda3e8cce703fb301d74fd5a6b60671a8303c
vi314759 <vinay.chidambara@wipro.com>
2017-09-01 20:50:50 +0530

    added config in assets

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -4,18 +4,21 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-
+import * as config from "assets/config.json";
 
 @Injectable()
 export class ConfigService
 {
 
-    // private _commonBaseURL: String = "http://l-185000861.wipro.com:8080/CommonV1/";
-    // private _helpline1097BaseURL: String = "http://l-185000861.wipro.com:8080/helpline1097APIV1/";
-    private _commonBaseURL: String = "http://10.152.3.99:8080/CommonV1/";
-    private _helpline1097BaseURL: String = "http://10.152.3.99:8080/helpline1097APIV1/";
+    private _commonBaseURL: String = "http://localhost:8080/CommonV1/";
+    private _helpline1097BaseURL: String = "http://localhost:8080/helpline1097APIV1/";
     private _telephonyServerURL: String = "http://10.201.13.17/";
     private _localeString = 'en-in';
+
+    constructor()
+    {
+        this.successHandeler( config );
+    }
 
     getCommonBaseURL ()
     {
@@ -33,5 +36,14 @@ export class ConfigService
     getLocaleString ()
     {
         return this._localeString;
+    }
+
+    successHandeler ( response )
+    {
+        // console.log( response, "config file obj" );
+        this._commonBaseURL = response.commonBaseURL;
+        this._helpline1097BaseURL = response.helpline1097BaseURL;
+        this._telephonyServerURL = response.telephonyServerURL;
+        this._localeString = response.localeString;
     }
 };
