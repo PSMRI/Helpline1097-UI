1017d181fb10c4d1929e8a1e757ad7fcf1273f1d
Pankush Manchanda (Digital) <PA298950@wipro.com>
2017-09-06 13:56:10 +0530

    Resolved issues

--- src/app/dashboard-navigation/dashboardNavigation.component.ts
+++ src/app/dashboard-navigation/dashboardNavigation.component.ts
@@ -1,7 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+import { ConfigService } from './../services/config/config.service';
 
 @Component({
     selector: 'dashboard-navigation',
     templateUrl: './dashboardNavigation.html',
 })
-export class DashboardNavigationComponent{}
+export class DashboardNavigationComponent {
+    loginUrl = this._config.getCommonLoginUrl();
+    constructor(private _config: ConfigService) {
+
+    }
+    changeRole() {
+        Cookie.deleteAll();
+        location.assign(this.loginUrl + '/MultiRoleScreenComponent');
+    }
+}
