3cd61f9b299f5ac16933514db1100802740b645f
unknown <DI352929@L-185002740.wipro.com>
2017-09-06 14:32:49 +0530

    serial no added in each table

--- src/app/beneficiary-registration/beneficiary-registration.component.ts
+++ src/app/beneficiary-registration/beneficiary-registration.component.ts
@@ -27,7 +27,7 @@ export class BeneficiaryRegistrationComponent implements OnInit
   @Input() onReloadCall: any;
   @Input() onStartNewCall: any;
   currentlanguage: any;
-
+  p=1;
 
   @Output() onBenRegDataSelect: EventEmitter<any> = new EventEmitter<any>();
   @Output() onBenSelect: EventEmitter<any> = new EventEmitter<any>();
