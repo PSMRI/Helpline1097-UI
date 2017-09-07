01cf0c75dc71c39cb2a7a9313b786b6fae184b33
vi314759 <vinay.chidambara@wipro.com>
2017-09-07 17:09:20 +0530

    changed code to read IP from config

--- src/app/set-password/set-password.component.ts
+++ src/app/set-password/set-password.component.ts
@@ -2,52 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { HttpServices } from '../services/http-services/http_services.service';
 import { dataService } from '../services/dataService/data.service';
 import { Router } from '@angular/router';
+import { ConfigService } from '../services/config/config.service';
 
+@Component( {
+	selector: 'app-set-password',
+	templateUrl: './set-password.component.html',
+	styleUrls: [ './set-password.component.css' ]
+} )
+export class SetPasswordComponent implements OnInit
+{
 
+	constructor(
+		public http_calls: HttpServices,
+		public getUserData: dataService,
+		public router: Router,
+		private configService: ConfigService
+	) { }
 
+	ngOnInit ()
+	{
+	}
 
-@Component({
-  selector: 'app-set-password',
-  templateUrl: './set-password.component.html',
-  styleUrls: ['./set-password.component.css']
-})
-export class SetPasswordComponent implements OnInit {
+	newpwd: any;
+	confirmpwd: any;
 
-	constructor(public http_calls: HttpServices, public getUserData: dataService, public router: Router) { }
+	uname: any = this.getUserData.uname;
+	private _commonBaseURL = this.configService.getCommonBaseURL();
 
-  ngOnInit() {
-  }
+	updatePassword ( new_pwd )
+	{
+		if ( new_pwd === this.confirmpwd )
+		{
+			this.http_calls.postData( this._commonBaseURL + "user/setForgetPassword",
+				{ "userName": this.uname, "password": new_pwd }
+			).subscribe(
+				( response: any ) => this.successCallback( response ),
+				( error: any ) => this.errorCallback( error )
+				);
 
-  newpwd: any;
-  confirmpwd: any;
+			alert( "password is changed for user " + this.uname + " wd new pwd as : " + new_pwd );
+		}
+		else
+		{
+			alert( "password dsnt match" );
+		}
+	}
 
-  uname: any = this.getUserData.uname;
+	successCallback ( response )
+	{
 
-
-  updatePassword(new_pwd) {
-	  if (new_pwd === this.confirmpwd) {
-			this.http_calls.postData(" http://10.152.3.152:1040/Common/user/setForgetPassword", 
-										{ "userName": this.uname, "password": new_pwd }
-									).subscribe(
-											(response: any) => this.successCallback(response),
-											(error: any) => this.errorCallback(error)
-												);
-		
-		alert("password is changed for user " + this.uname + " wd new pwd as : " + new_pwd);
-	  }
-	  else {
-		  alert("password dsnt match");
-	  }
-  }
-
-  successCallback(response) {
-
-	  console.log(response);
-	  this.router.navigate(['/loginContentClass']);
-  }
-  errorCallback(response) {
-	  console.log(response);
-  }
+		console.log( response );
+		this.router.navigate( [ '/loginContentClass' ] );
+	}
+	errorCallback ( response )
+	{
+		console.log( response );
+	}
 
 
 }
