3fadcc0f727b7b0cf4d2fe3bc30ec64f64ab1b27
Pankush Manchanda (Digital) <PA298950@wipro.com>
2017-09-20 11:59:49 +0530

    Security Issue Fixed

--- src/app/set-security-questions/set-security-questions.component.ts
+++ src/app/set-security-questions/set-security-questions.component.ts
@@ -10,9 +10,27 @@ import { ConfigService } from '../services/config/config.service';
   styleUrls: ['./set-security-questions.component.css']
 })
 export class SetSecurityQuestionsComponent implements OnInit {
+  question1: any = '';
+  question2: any = '';
+  question3: any = '';
 
-  constructor(public getUserData: dataService, public http_calls: HttpServices, public router: Router, private configService: ConfigService) {
+  answer1: any = '';
+  answer2: any = '';
+  answer3: any = '';
 
+  questions: any = [];
+  selectedQuestions: any = [];
+
+  passwordSection: boolean = false;
+  questionsection: boolean = true;
+  uname: any = this.getUserData.uname;
+  uid: any;
+  dataArray: any = [];
+  oldpwd: any;
+  newpwd: any;
+  confirmpwd: any;
+  constructor(public getUserData: dataService, public http_calls: HttpServices
+    , public router: Router, private configService: ConfigService) {
   }
 
   handleSuccess(response) {
@@ -24,45 +42,32 @@ export class SetSecurityQuestionsComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    this.http_calls.getData(this.configService.getCommonBaseURL() + "user/getsecurityquetions").subscribe(
+    this.uid = this.getUserData.uid;
+    this.http_calls.getData(this.configService.getCommonBaseURL() + 'user/getsecurityquetions').subscribe(
       (response: any) => this.handleSuccess(response),
       (error: any) => this.handleError(error));
 
   }
 
-  uid: any = this.getUserData.uid;
-  passwordSection: boolean = false;
-  questionsection: boolean = true;
-  uname: any = this.getUserData.uname;
-
   switch() {
     this.passwordSection = true;
     this.questionsection = false;
   }
-  question1: any = "";
-  question2: any = "";
-  question3: any = "";
 
-  answer1: any = "";
-  answer2: any = "";
-  answer3: any = "";
-
-  questions: any = [];
-  selectedQuestions: any = [];
 
   updateQuestions(selectedques: any) {
 
-    if (this.selectedQuestions.indexOf(selectedques) == -1) {
+    if (selectedques) {
       this.selectedQuestions.push(selectedques);
+      this.questions = this.questions.filter(val => !(this.selectedQuestions.includes(val.QuestionID)));
       // this.questions.splice(this.questions.indexOf(selectedques), 1);
+    } else {
+      alert('Please select a question');
     }
-    else {
-      alert('choose a different question... this question is already selected');
-    }
+
   }
 
-  dataArray: any = [];
+
 
   setSecurityQuestions() {
 
@@ -71,25 +76,25 @@ export class SetSecurityQuestionsComponent implements OnInit {
     if (this.selectedQuestions.length == 3) {
 
       this.dataArray = [{
-        "userID": this.uid,
-        "questionID": this.question1,
-        "answers": this.answer1,
-        "mobileNumber": "1234567890",
-        "createdBy": "neeraj"
+        'userID': this.uid,
+        'questionID': this.question1,
+        'answers': this.answer1,
+        'mobileNumber': '1234567890',
+        'createdBy': 'neeraj'
       },
       {
-        "userID": this.uid,
-        "questionID": this.question2,
-        "answers": this.answer2,
-        "mobileNumber": "1234567890",
-        "createdBy": "neeraj"
+        'userID': this.uid,
+        'questionID': this.question2,
+        'answers': this.answer2,
+        'mobileNumber': '1234567890',
+        'createdBy': 'neeraj'
       },
       {
-        "userID": this.uid,
-        "questionID": this.question3,
-        "answers": this.answer3,
-        "mobileNumber": "1234567890",
-        "createdBy": "neeraj"
+        'userID': this.uid,
+        'questionID': this.question3,
+        'answers': this.answer3,
+        'mobileNumber': '1234567890',
+        'createdBy': 'neeraj'
       }
       ]
       // this.dataArray = {
@@ -112,13 +117,13 @@ export class SetSecurityQuestionsComponent implements OnInit {
       // alert("the data set is :" + this.dataObj);
       console.log(this.selectedQuestions);
 
-      this.http_calls.postData(this.configService.getCommonBaseURL() + "user/saveUserSecurityQuesAns", this.dataArray).subscribe(
+      this.http_calls.postData(this.configService.getCommonBaseURL() + 'user/saveUserSecurityQuesAns', this.dataArray).subscribe(
         (response: any) => this.handleQuestionSaveSuccess(response),
         (error: any) => this.handleQuestionSaveError(error));
 
     }
     else {
-      alert("all 3 ques shud be diff");
+      alert('all 3 ques shud be diff');
     }
 
 
@@ -130,26 +135,24 @@ export class SetSecurityQuestionsComponent implements OnInit {
 
   }
   handleQuestionSaveError(response) {
-    console.log("question save error", response);
+    console.log('question save error', response);
   }
 
-  oldpwd: any;
-  newpwd: any;
-  confirmpwd: any;
+
 
   updatePassword(new_pwd) {
     if (new_pwd === this.confirmpwd) {
 
 
-      this.http_calls.postData(this.configService.getCommonBaseURL() + "user/setForgetPassword", { "userName": this.uname, "password": new_pwd }).
+      this.http_calls.postData(this.configService.getCommonBaseURL() + 'user/setForgetPassword', { 'userName': this.uname, 'password': new_pwd }).
         subscribe(
         (response: any) => this.successCallback(response),
         (error: any) => this.errorCallback(error));
 
-      alert("password is changed for user " + this.uname + " wd new pwd as : " + new_pwd);
+      alert('password is changed for user ' + this.uname + ' wd new pwd as : ' + new_pwd);
     }
     else {
-      alert("password dsnt match");
+      alert('password dsnt match');
     }
   }
 
