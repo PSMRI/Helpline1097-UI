3fe69b087ec2aa27f6f5794f8f8c12742e58278c
Gursimran Singh (Digital) <GU353199@wipro.com>
2017-11-28 18:40:01 +0530

    Reports Excel downloading implemented

--- src/app/caller-age-report/caller-age-report.component.ts
+++ src/app/caller-age-report/caller-age-report.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserBeneficiaryData } from '../services/common/userbeneficiarydata.service'
+import { dataService } from '../services/dataService/data.service';
+import { LocationService } from '../services/common/location.service';
+import { ReportsService } from '../services/reports-service/reports-service';
+import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 
 @Component({
   selector: 'app-caller-age-report',
@@ -11,20 +16,71 @@ export class CallerAgeReportComponent implements OnInit {
   start_date: Date;
   end_date: Date;
   minStartDate: Date;
-  tableFlag: boolean = true;
+  tableFlag: boolean = false;
   data = {};
-
-  constructor() { }
+  states = [];
+  state: any;
+  districts = [];
+  district: any;
+  ageGroup: any;
+  ageGroups = [];
+  constructor(private _userBeneficiaryData: UserBeneficiaryData, private saved_data: dataService,
+     private _locationService: LocationService, private reportService: ReportsService) { }
 
   ngOnInit() {
     this.today = new Date();
     console.log(this.today);
-    this.end_date = this.today;
+    this.end_date = new Date();
+    this.end_date.setDate(this.today.getDate()-1);
     this.start_date = new Date();
     this.start_date.setDate(this.today.getDate()-7);
     this.minStartDate = new Date();
     this.minStartDate.setMonth(this.minStartDate.getMonth()-1);
     //call api and initialize data
+    this._userBeneficiaryData.getUserBeneficaryData(this.saved_data.current_service.serviceID)
+    .subscribe((response) => {
+      this.SetUserBeneficiaryRegistrationData(response)
+    },
+    (err) => {
+
+    });
+    this.ageGroups = [
+      {
+        "ageGroupDisplay": "Below 15",
+        "ageGroupValue": {
+          "minAge": 0,
+          "maxAge": 15
+        }
+      },
+      {
+        "ageGroupDisplay": "15 to 24",
+        "ageGroupValue": {
+          "minAge": 15,
+          "maxAge": 24
+        }
+      },
+      {
+        "ageGroupDisplay": "24 to 39",
+        "ageGroupValue": {
+          "minAge": 24,
+          "maxAge": 39
+        }
+      },
+      {
+        "ageGroupDisplay": "40 to 59",
+        "ageGroupValue": {
+          "minAge": 40,
+          "maxAge": 59
+        }
+      },
+      {
+        "ageGroupDisplay": "Above 59",
+        "ageGroupValue": {
+          "minAge": 59,
+          "maxAge": 120
+        }
+      }
+    ]
   }
 
   blockKey(e: any){
@@ -44,9 +100,62 @@ export class CallerAgeReportComponent implements OnInit {
     this.start_date.setMonth(this.start_date.getMonth()-1);
   }
 
-  getReports(){
-    //call api and initialize data
-    // this.tableFlag = true;
-  }
+  getReports(value) {
 
+    let noOfGroups = value.ageGroup.length;
+    let array = [];
+    let obj = {};
+
+    let start_date = new Date((value.startDate) - 1 * (value.startDate.getTimezoneOffset() * 60 * 1000)).toJSON().slice(0, 10) + "T00:00:00.000Z"; 
+    let end_date = new Date((value.startDate) - 1 * (value.startDate.getTimezoneOffset() * 60 * 1000)).toJSON().slice(0, 10) + "T00:00:00.000Z"; 
+
+    for(let i=0; i<noOfGroups; i++) {
+      obj = {
+        "maxAge": value.ageGroup[i].maxAge,
+        "minAge" : value.ageGroup[i].minAge,
+        "startTimestamp": start_date,
+        "endTimestamp": end_date,
+        "beneficiaryState": this.state.stateName,
+        "beneficiaryDistrict": value.district
+      }
+      array.push(obj);
+    }
+    console.log(array);
+    this.reportService.getAllByAgeGroup(array).subscribe((response)=>{ this.reportSuccessHandle(response)}, (err) => { });
+  }
+  count = [];
+  reportSuccessHandle(res) {
+    console.log(res);
+    this.tableFlag = true;
+    this.count = res;
+  }
+  SetUserBeneficiaryRegistrationData(response) {
+    const regData = response;
+    if (regData.states) {
+      this.states = regData.states;
+    }
+  }
+  GetDistricts(state: any) {
+    this.districts = [];
+    this.district = undefined;
+    if (state) {
+      this._locationService.getDistricts(state.stateID)
+      .subscribe((response) => this.SetDistricts(response), (err) => { });
+    }
+  }
+  SetDistricts(response: any) {
+    this.districts = response;
+  }
+  download() {
+      var options = { 
+   
+    showLabels: true, 
+    showTitle: true
+
+  };
+  
+   let head = Object.keys(this.count[0]);
+   // console.log(head);
+        new Angular2Csv(this.count, 'AgeGroup Report',options);
+  }
 }
