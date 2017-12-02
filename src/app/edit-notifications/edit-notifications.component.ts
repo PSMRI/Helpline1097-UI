9667766bb1538ed273ffd954c587e4d689aeabc1
unknown <DI352929@L-185002740.wipro.com>
2017-12-02 13:50:21 +0530

    file extension picking up code updated

--- src/app/edit-notifications/edit-notifications.component.ts
+++ src/app/edit-notifications/edit-notifications.component.ts
@@ -76,7 +76,8 @@ export class EditNotificationsComponent implements OnInit {
     var count = 0;
     console.log("FILE DETAILS", file);
     if (file) {
-      var file_extension = file.name.split(".")[1];
+      var array_after_split=file.name.split(".");
+    var file_extension=array_after_split[array_after_split.length-1];
       for (let i = 0; i < this.valid_file_extensions.length; i++) {
         if (file_extension.toUpperCase() === this.valid_file_extensions[i].toUpperCase()) {
           count = count + 1;
