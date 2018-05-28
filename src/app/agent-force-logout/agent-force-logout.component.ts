17bb01dbf27b5c2d73f6406da2b5769e8a0a9550
Diamond Khanna (Digital) <DI352929@wipro.com>
2018-05-28 18:15:12 +0530

    agent force logout wip; force logout changes in supervisor; co info services changes

---
+++ src/app/agent-force-logout/agent-force-logout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-force-logout',
+  templateUrl: './agent-force-logout.component.html',
+  styleUrls: ['./agent-force-logout.component.css']
+})
+export class AgentForceLogoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
