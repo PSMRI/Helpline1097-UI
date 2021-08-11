f632f645d9fdaa390d7654a976dcb0f61e7316e3
Devika S (APMEA - iDEAS-AEM) <DE40034072@wipro.com>
2021-08-11 19:23:19 +0530

    Common Dialog issue resolved

--- src/app/common-dialog/common-dialog.component.ts
+++ src/app/common-dialog/common-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Injector } from '@angular/core';
 import { MdDialogRef } from '@angular/material';
 import { HttpServices } from "../services/http-services/http_services.service";
 import { SetLanguageComponent } from 'app/set-language.component';
@@ -19,8 +19,11 @@ export class CommonDialogComponent implements OnInit {
   public remarks: boolean;
   public status: string;
   currentLanguageSet: any;
+  httpServices: HttpServices;
 
-  constructor(public dialogRef: MdDialogRef<CommonDialogComponent>,public HttpServices: HttpServices) { }
+  constructor(public dialogRef: MdDialogRef<CommonDialogComponent>,private injector: Injector) {
+    this.httpServices = injector.get(HttpServices);
+   }
 
   ngOnInit() {
     this.assignSelectedLanguage();
@@ -32,7 +35,7 @@ export class CommonDialogComponent implements OnInit {
     this.assignSelectedLanguage();
   }
   assignSelectedLanguage() {
-    const getLanguageJson = new SetLanguageComponent(this.HttpServices);
+    const getLanguageJson = new SetLanguageComponent(this.httpServices);
     getLanguageJson.setLanguage();
     this.currentLanguageSet = getLanguageJson.currentLanguageObject;
     }
