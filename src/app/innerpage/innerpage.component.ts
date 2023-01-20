91dbaf8b95ed40147beb6d1aabb7f9b742b69151
Devika S <DE40034072@wipro.com>
2023-01-20 18:20:07 +0530

    Minor change with next button

--- src/app/innerpage/innerpage.component.ts
+++ src/app/innerpage/innerpage.component.ts
@@ -873,7 +873,7 @@ export class InnerpageComponent implements OnInit {
     jQuery("#four").find("a").addClass("active-tab");
     jQuery("#btnClosure").attr("disabled", "disabled");
     // jQuery("#btnCancel").attr("disabled", "disabled");
-    jQuery("#next").hide();
+    // jQuery("#next").hide();
     jQuery("#previous").show();
 
     this.getCommonData.enablePreviousOnCustDisconnect(true);
