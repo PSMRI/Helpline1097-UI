0d1925599f73882283ce761fe5f63ffe3812de86
swetatech24 <sweta.prakash@wipro.com>
2023-09-25 10:48:51 +0530

    removing build issues

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -81,7 +81,7 @@ export class loginContentClass implements OnInit, OnDestroy {
         this.dataSettingService.Userdata = response;
         // this.dataSettingService.userPriveliges = response.Previlege;
         if(response.previlegeObj !== undefined && response.previlegeObj !== null) {
-        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097"; });
+        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097" });
         }
         
         // if (this.previlageObj.length > 0) {
@@ -107,7 +107,8 @@ export class loginContentClass implements OnInit, OnDestroy {
       });
     }
 
-  }
+  };
+
   ngOnInit() {
     this.httpService.dologoutUsrFromPreSession(false);
     this.logoutUserFromPreviousSessionSubscription =
@@ -122,7 +123,7 @@ export class loginContentClass implements OnInit, OnDestroy {
       this.loginservice.checkAuthorisedUser().subscribe((response) => {
         if(response !== undefined && response !== null) {
           if(response.previlegeObj !== undefined && response.previlegeObj !== null) {
-          this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097"; });
+          this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097" });
         }
         // if (this.previlageObj.length > 0) {
         this.dataSettingService.Userdata = response;
@@ -206,7 +207,7 @@ export class loginContentClass implements OnInit, OnDestroy {
       hasher: CryptoJS.algo.SHA512,
       keySize: this.keySize / 32,
       iterations: this._iterationCount
-    });
+    })
   }
 
 
@@ -232,37 +233,12 @@ export class loginContentClass implements OnInit, OnDestroy {
         console.error('Error hashing password:', err);
       } else {
         this.encryptPassword = hashedPassword;
-    this.loginservice
-      .authenticateUser(this.userID, this.encryptPassword, doLogOut)
-      .subscribe(
-        (response: any) => {
-          if (
-            response !== undefined &&
-            response !== null &&
-            response.previlegeObj !== undefined &&
-            response.previlegeObj !== null
-          ) {
-            this.successCallback(response, this.userID, this.password);
-          }
-        },
-        (error: any) => this.errorCallback(error)
-        );
-      }
-    });
-  }
-
-  
-  loginUser(doLogOut) {
-    this.loginservice
-    .userLogOutFromPreviousSession(this.userID)
-    .subscribe(
-      (userLogOutRes: any) => {
-      if(userLogOutRes && userLogOutRes.data.response) {
-        bcrypt.hash(this.password, 12, (err, hashedPassword) => {
-          if (err) {
-            console.error('Error hashing password:', err);
-          } else {
-            this.encryptPassword = hashedPassword;
+  //   this.password = CryptoJS.AES.encrypt(this.password,this.encPassword).toString();
+  //  console.log("PARTH"+this.password.ciphertext.toString(CryptoJS.enc.Base64))
+    // this.password = AES.encrypt(this.password).toString();
+    // this.password = CryptoJS.SHA256(this.password).toString();
+    // this.encryptedVar=SHA256(this.password).toString(enc.Hex);
+    // this.password=this.encryptedVar.substr(0, 16);
     this.loginservice
       .authenticateUser(this.userID, this.encryptPassword, doLogOut)
       .subscribe(
@@ -280,14 +256,62 @@ export class loginContentClass implements OnInit, OnDestroy {
       );
     }
   });
+  }
+
+  // login(doLogOut) {
+  //   this.loginservice
+  //     .authenticateUser(this.userID, this.password, doLogOut)
+  //     .subscribe(
+  //       (response: any) => {
+  //         if (
+  //           response !== undefined &&
+  //           response !== null &&
+  //           response.previlegeObj !== undefined &&
+  //           response.previlegeObj !== null
+  //         ) {
+  //           this.successCallback(response, this.userID, this.password);
+  //         }
+  //       },
+  //       (error: any) => this.errorCallback(error)
+  //     );
+  // }
+//ADID- KA40094929 
+// added new method to force logout the user
+  loginUser(doLogOut) {
+    this.loginservice
+    .userLogOutFromPreviousSession(this.userID)
+    .subscribe(
+      (userLogOutRes: any) => {
+      if(userLogOutRes && userLogOutRes.data.response) {
+        bcrypt.hash(this.password, 12, (err, hashedPassword) => {
+          if (err) {
+            console.error('Error hashing password:', err);
+          } else {
+            this.encryptPassword = hashedPassword;
+        this.loginservice
+      .authenticateUser(this.userID, this.encryptPassword, doLogOut)
+      .subscribe(
+        (response: any) => {
+          if (
+            response !== undefined &&
+            response !== null &&
+            response.previlegeObj !== undefined &&
+            response.previlegeObj !== null
+          ) {
+            this.successCallback(response, this.userID, this.password);
+          }
+        },
+        (error: any) => this.errorCallback(error)
+      );
       }
+    });
+  } 
       else
       {
             this.alertService.alert(userLogOutRes.errorMessage, 'error');
-          }
-        }
-      );
-    }
+      }
+      });
+  }
 
   successCallback(response: any, userID: any, password: any) {
     this.dataSettingService.current_campaign=undefined;
@@ -295,7 +319,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     console.log(response);
     if (response !== undefined && response !== null) {
       if(response.previlegeObj !== undefined && response.previlegeObj !== null) {
-        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097"; });
+        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097" });
       }
     // if (this.previlageObj.length > 0) {
     this.dataSettingService.Userdata = response;
@@ -308,7 +332,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     this.dataSettingService.uname = response.userName;
     this.previlageObj.forEach((assignAgentID) => {
       this.dataSettingService.Userdata.agentID = assignAgentID.agentID;
-    });
+    })
     this.dataSettingService.loginIP = response.loginIPAddress;
     // this.getLoginKey(userID, password);
     // console.log( "array" + response.Previlege );
@@ -336,7 +360,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     // }
     this.dataSettingService.setInboundOutboundValue(response);
   }
-  }
+  };
   errorCallback(error: any) {
     if (error.status) {
       this.loginResult = error.errorMessage;
@@ -345,7 +369,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     }
     // this.loading = false;
     console.log(error);
-  }
+  };
   getLoginKey(userId, password) {
     this.czentrixServices.getLoginKey(userId, password).subscribe((response) => {
       console.log('getLoginKey response: ' + JSON.stringify(response));
@@ -354,7 +378,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     }, (err) => {
       this.alertService.alert(err.errorMessage, 'error');
       console.log('Error in getLoginKey', err);
-    });
+    })
 
 
   }
