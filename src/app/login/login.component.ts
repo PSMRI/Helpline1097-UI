5f4477fe1b2190db8996a4bc5f1ad29c69801e8a
swetatech24 <sweta.prakash@wipro.com>
2023-09-25 09:43:52 +0530

    improving changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -81,7 +81,7 @@ export class loginContentClass implements OnInit, OnDestroy {
         this.dataSettingService.Userdata = response;
         // this.dataSettingService.userPriveliges = response.Previlege;
         if(response.previlegeObj !== undefined && response.previlegeObj !== null) {
-        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097"; });
+        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097" });
         }
         
         // if (this.previlageObj.length > 0) {
@@ -107,7 +107,7 @@ export class loginContentClass implements OnInit, OnDestroy {
       });
     }
 
-  }
+  };
   ngOnInit() {
     this.httpService.dologoutUsrFromPreSession(false);
     this.logoutUserFromPreviousSessionSubscription =
@@ -122,7 +122,7 @@ export class loginContentClass implements OnInit, OnDestroy {
       this.loginservice.checkAuthorisedUser().subscribe((response) => {
         if(response !== undefined && response !== null) {
           if(response.previlegeObj !== undefined && response.previlegeObj !== null) {
-          this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097"; });
+          this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097" });
         }
         // if (this.previlageObj.length > 0) {
         this.dataSettingService.Userdata = response;
@@ -206,7 +206,7 @@ export class loginContentClass implements OnInit, OnDestroy {
       hasher: CryptoJS.algo.SHA512,
       keySize: this.keySize / 32,
       iterations: this._iterationCount
-    });
+    })
   }
 
 
@@ -295,7 +295,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     console.log(response);
     if (response !== undefined && response !== null) {
       if(response.previlegeObj !== undefined && response.previlegeObj !== null) {
-        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097"; });
+        this.previlageObj = response.previlegeObj.filter((previlage) => { return previlage.serviceName == "1097" });
       }
     // if (this.previlageObj.length > 0) {
     this.dataSettingService.Userdata = response;
@@ -308,7 +308,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     this.dataSettingService.uname = response.userName;
     this.previlageObj.forEach((assignAgentID) => {
       this.dataSettingService.Userdata.agentID = assignAgentID.agentID;
-    });
+    })
     this.dataSettingService.loginIP = response.loginIPAddress;
     // this.getLoginKey(userID, password);
     // console.log( "array" + response.Previlege );
@@ -345,7 +345,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     }
     // this.loading = false;
     console.log(error);
-  }
+  };
   getLoginKey(userId, password) {
     this.czentrixServices.getLoginKey(userId, password).subscribe((response) => {
       console.log('getLoginKey response: ' + JSON.stringify(response));
@@ -354,7 +354,7 @@ export class loginContentClass implements OnInit, OnDestroy {
     }, (err) => {
       this.alertService.alert(err.errorMessage, 'error');
       console.log('Error in getLoginKey', err);
-    });
+    })
 
 
   }
