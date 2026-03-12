202eacdfc70acc7732b184f97f9ba2b346fc67a4
SnehaRH <sneha@navadhiti.com>
2026-03-12 11:01:21 +0530

    fix: AMM-2196 breakfree india feild needs to be added

--- src/app/updates-from-beneficiary/updates-from-beneficiary.component.ts
+++ src/app/updates-from-beneficiary/updates-from-beneficiary.component.ts
@@ -66,7 +66,8 @@ export class UpdatesFromBeneficiaryComponent implements OnInit, DoCheck {
     { name: 'Healthcare Worker', value: 'Healthcare Worker', selected: false, id: 5, disabled: false },
     { name: 'Others', value: 'Others', selected: false, id: 6, disabled: false },
     { name: 'Newspaper', value: 'Newspaper', selected: false, id: 8, disabled: false },
-    { name: 'Not Disclosed', value: 'Not Disclosed', selected: false, id: 7, disabled: false }
+    { name: 'Not Disclosed', value: 'Not Disclosed', selected: false, id: 7, disabled: false },
+    { name: 'Breakfree India', value: 'Breakfree India', selected: false, id: 9, disabled: false }
   ];
   currentLanguageSet: any;
 
