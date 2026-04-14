22f25ade6a12109d031a14dc6c7d9b6ab50617b9
vishwab1 <vishwanath@navadhiti.com>
2026-04-14 17:53:13 +0530

    fix:feedback change

--- src/app/services/coService/co_feedback.service.ts
+++ src/app/services/coService/co_feedback.service.ts
@@ -1,8 +1,8 @@
-/* 
-* AMRIT – Accessible Medical Records via Integrated Technology 
-* Integrated EHR (Electronic Health Records) Solution 
+/*
+* AMRIT – Accessible Medical Records via Integrated Technology
+* Integrated EHR (Electronic Health Records) Solution
 *
-* Copyright (C) "Piramal Swasthya Management and Research Institute" 
+* Copyright (C) "Piramal Swasthya Management and Research Institute"
 *
 * This file is part of AMRIT.
 *
@@ -34,64 +34,64 @@ import { AuthorizationWrapper } from './../../authorization.wrapper';
 @Injectable()
 export class CoFeedbackService {
 
-    test = [];
-    _baseurl = this._config.get1097BaseURL();
-    _commonUrl = this._config.getCommonBaseURL();
-    _servicetypesurl = this._commonUrl + 'service/servicetypes';
-    // _servicetypesurl = this._baseurl + 'api/helpline1097/co/get/servicetypes'
-    _createFeedbackURL = this._baseurl + 'co/saveBenFeedback'
-    _getDesignationsURL = this._baseurl + 'designation/get'
-    // _getFeedbackHistoryByID = this._baseurl + 'services/getFeedbacksHistory'
-    _getFeedbackHistory = this._commonUrl + 'feedback/getFeedbacksList';
-     saveComplaintResolutionURL = this._commonUrl + 'saveComplaintResolution'
-    constructor(
-        private _http: AuthorizationWrapper,
-        private _config: ConfigService,
-        private _httpInterceptor: InterceptedHttp
-    ) { }
+  test = [];
+  _baseurl = this._config.get1097BaseURL();
+  _commonUrl = this._config.getCommonBaseURL();
+  _servicetypesurl = this._commonUrl + 'service/servicetypes';
+  // _servicetypesurl = this._baseurl + 'api/helpline1097/co/get/servicetypes'
+  _createFeedbackURL = this._baseurl + 'co/saveBenFeedback'
+  _getDesignationsURL = this._baseurl + 'designation/get'
+  // _getFeedbackHistoryByID = this._baseurl + 'services/getFeedbacksHistory'
+  _getFeedbackHistory = this._commonUrl + 'feedback/getFeedbacksList';
+  saveComplaintResolutionURL = this._commonUrl + 'saveComplaintResolution'
+  constructor(
+    private _http: AuthorizationWrapper,
+    private _config: ConfigService,
+    private _httpInterceptor: InterceptedHttp
+  ) { }
 
-    getTypes(providerServiceMapID: number) {
-        let data = {};
-        data['providerServiceMapID'] = providerServiceMapID;
-        return this._http.post(this._servicetypesurl, data)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
-    createFeedback(data: any) {
-        return this._httpInterceptor.post(this._createFeedbackURL, data)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
+  getTypes(providerServiceMapID: number) {
+    let data = {};
+    data['providerServiceMapID'] = providerServiceMapID;
+    return this._http.post(this._servicetypesurl, data)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+  createFeedback(data: any) {
+    return this._httpInterceptor.post(this._createFeedbackURL, data)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
 
-    getDesignations() {
-        let data = {};
-        return this._http.post(this._getDesignationsURL, data)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
+  getDesignations() {
+    let data = {};
+    return this._http.post(this._getDesignationsURL, data)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
 
-    getFeedbackHistoryById(id: any, serviceID: any) {
-        return this._http.post(this._getFeedbackHistory, { 'beneficiaryRegID': id, 'serviceID': serviceID })
-            .map(this.extractData).catch(this.handleError);
-    }
+  getFeedbackHistoryById(id: any, serviceID: any) {
+    return this._http.post(this._getFeedbackHistory, { 'beneficiaryRegID': id, 'serviceID': serviceID, 'is1097': true })
+      .map(this.extractData).catch(this.handleError);
+  }
 
-    saveComplaintResolution(data: any) {
-        return this._httpInterceptor.post(this.saveComplaintResolutionURL, data)
-            .map(this.extractData)
-            .catch(this.handleError);
-    }
+  saveComplaintResolution(data: any) {
+    return this._httpInterceptor.post(this.saveComplaintResolutionURL, data)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
 
-    extractData(response: Response) {
-        if (response.json().data) {
-            return response.json().data;
-        } else {
-            return Observable.throw(response.json());
-        }
+  extractData(response: Response) {
+    if (response.json().data) {
+      return response.json().data;
+    } else {
+      return Observable.throw(response.json());
     }
+  }
 
-    handleError(response: Response) {
-        return Observable.throw(response.json());
-    }
+  handleError(response: Response) {
+    return Observable.throw(response.json());
+  }
 }
 
 
